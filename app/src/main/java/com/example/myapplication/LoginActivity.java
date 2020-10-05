3e866d58030898362478a8a6c463ba8be218f92c
DoHu-ng <donghunglig009@gmail.com>
2020-10-05 23:06:50 +0700

    Lab 1 new

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,6 +24,53 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin=findViewById(R.id.btnLogin);
         btnRegister=findViewById(R.id.btnRegister);
 
+        /*btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editUsername.getText().toString().isEmpty()||editPassword.getText().toString().isEmpty()){
+                    final Dialog dialog=new Dialog(LoginActivity.this);
+                    dialog.setContentView(R.layout.dialog_custom);
+                    btnOK = dialog.findViewById(R.id.btnOK);
+                    btnCancel = dialog.findViewById(R.id.btnCancel);
+                    btnOK.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent intent = new Intent(LoginActivity.this,
+                                    RegisterActivity.class);
+                            startActivityForResult(intent, 100);
+                            dialog.dismiss();
+                        }
+                    });
+                    btnCancel.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dialog.cancel();
+                        }
+                    });
 
+                    dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,
+                            WindowManager.LayoutParams.WRAP_CONTENT);
+                    dialog.show();
+                }else if(edtPassword.getText().toString().length() < 6){
+                    edtPassword.setError("Minimum 6 number");
+                }else {
+                    Intent intent = new Intent(LoginActivity.this,
+                            InfoActivity.class);
+                    intent.putExtra("Username", edtUsername.getText().toString());
+                    startActivity(intent);
+                }
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,
+                        RegisterActivity.class);
+                startActivityForResult(intent, 100);
+
+                }
+
+
+        });*/
     }
 }
